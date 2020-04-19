488c3cd3b4409d8b029c27adc4060626565c3e0e
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-19 20:53:23 +0100

    Add ability to split funds to Splitter.sol

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -5,7 +5,11 @@ contract Splitter {
     address public alice;
     address public bob;
     address public carol;
-    
+
+    uint private aliceDeposite;
+    uint private bobFunds;
+    uint private carolFunds;
+
     constructor () public {
         owner = msg.sender;
     }
@@ -29,5 +33,39 @@ contract Splitter {
         
         carol = _carol;
         return true;
+    }
+
+    function SplitFundFromAlice() public payable returns(uint) {
+        require(alice == msg.sender && msg.value > 0);
+        uint oneWei = 1;
+        uint amount = msg.value;
+        uint splitForBob = amount / 2;
+        uint splitForCarol = amount / 2;
+        if (amount % 2 == 1){
+            splitForBob += oneWei;
+        }
+
+        require(amount == splitForBob + splitForCarol);
+
+        aliceDeposite += amount;
+        bobFunds += splitForBob;
+        carolFunds += splitForCarol;
+        return amount;
+    }
+
+    function getAliceDeposit() public view returns (uint){
+        return aliceDeposite;
+    }
+
+    function getBobFunds() public view returns (uint){
+        return bobFunds;
+    }
+
+    function getCarolFunds() public view returns (uint){
+        return carolFunds;
+    }
+
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
     }
 }
