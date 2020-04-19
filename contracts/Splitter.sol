5ec4e9b54dd361ec1e54f65b1cde4982fb3375ce
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-19 13:14:41 +0100

    Modify Register functions on Splitter.sol
    The registration of users of cantract has the folowing rules:
    First Register Alice, then Register Bob, finally Register Carol. Alice
    can be the same as the owner of the contract. Bob cannot be the same as
    Alice or owner. Caral cannot be the same as Alice, Bob or owner.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,28 +4,28 @@ contract Splitter {
     address public owner;
     address public alice;
     address public bob;
-    address public carol
+    address public carol;
     
     constructor () public {
         owner = msg.sender;
     }
     
     function RegisterAlice(address _alice) public returns (bool success){
-        require (alice = address(0));
+        require (alice == address(0) && bob == address(0) && carol == address(0));
         
         alice = _alice;
         return true;
     }
     
     function RegisterBob(address _bob) public returns (bool success){
-        require (bob = address(0));
+        require (owner != _bob && alice != _bob && alice != address(0) && bob == address(0) && carol == address(0));
         
         bob = _bob;
         return true;
     }
     
     function RegisterCarol(address _carol) public returns (bool success){
-        require (bob = address(0));
+        require (owner != _carol && alice != _carol && bob != _carol && alice != address(0) && bob != address(0) && carol == address(0));
         
         carol = _carol;
         return true;
