72f6d85088f9c043fd98cbe5d2c51cb684f8e310
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-21 00:33:21 +0100

    Remove variable owner from contract
    The variable 'address public owner' is not required in this contract.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.5.0;
 
 contract Splitter {
-    address public owner;
     address public alice;
     address public bob;
     address public carol;
@@ -14,10 +13,6 @@ contract Splitter {
     event LogRegisterCarol(address sender, address carol);
     event LogSplitFunds(address sender, uint amount_from_alice, uint amount_to_bob, uint amount_to_carol);
 
-    constructor () public {
-        owner = msg.sender;
-    }
-
     function registerAlice(address _alice) public returns (bool success){
         require (alice == address(0) && bob == address(0) && carol == address(0), 'failed to register Alice');
         emit LogRegisterAlice(msg.sender, _alice);
@@ -26,14 +21,14 @@ contract Splitter {
     }
     
     function registerBob(address _bob) public returns (bool success){
-        require (owner != _bob && alice != _bob && alice != address(0) && bob == address(0) && carol == address(0), 'failed to register Bob');
+        require (alice != _bob && alice != address(0) && bob == address(0) && carol == address(0), 'failed to register Bob');
         emit LogRegisterBob(msg.sender, _bob);
         bob = _bob;
         return true;
     }
     
     function registerCarol(address _carol) public returns (bool success){
-        require (owner != _carol && alice != _carol && bob != _carol && alice != address(0) && bob != address(0) && carol == address(0), 'failed to register Carol');
+        require (alice != _carol && bob != _carol && alice != address(0) && bob != address(0) && carol == address(0), 'failed to register Carol');
         emit LogRegisterCarol(msg.sender, _carol);
         carol = _carol;
         return true;
--- test/splitter.test.js
+++ test/splitter.test.js
@@ -3,12 +3,10 @@ const Splitter = artifacts.require("Splitter");
 contract('Splitter - Initialization', (accounts) => {
     it('Should initialize', async () => {
         const splitterInstance = await Splitter.deployed();
-        const owner = await splitterInstance.owner();
         const alice = await splitterInstance.alice();
         const bob = await splitterInstance.bob();
         const carol = await splitterInstance.carol();
         const ZeroAddress = '0x0000000000000000000000000000000000000000';
-        assert.equal(owner, accounts[0], 'initial owner is incorrect');
         assert.equal(alice, ZeroAddress, 'initial alice is incorrect');
         assert.equal(bob, ZeroAddress, 'initial bob is incorrect');
         assert.equal(carol, ZeroAddress, 'initial carol is incorrect');
