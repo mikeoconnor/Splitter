98d1fa8c1481a1e14ebe11714c2f7156ac68f101
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-21 02:00:06 +0100

    Redesign (simplification) of contract interface
    The interface of the contract has been simplified. There is no longer
    any need to register the participants. Balances are now stored in
    'mapping (address => uint) public balances', thus removing the need for
    duplicated code.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,74 +1,38 @@
 pragma solidity 0.5.0;
 
 contract Splitter {
-    address public alice;
-    address public bob;
-    address public carol;
 
-    uint public bobFunds;
-    uint public carolFunds;
+    mapping (address => uint) public balances;
 
-    event LogRegisterAlice(address sender, address alice);
-    event LogRegisterBob(address sender, address bob);
-    event LogRegisterCarol(address sender, address carol);
     event LogSplitFunds(address sender, uint amount_from_alice, uint amount_to_bob, uint amount_to_carol);
 
-    function registerAlice(address _alice) public returns (bool success){
-        require (alice == address(0) && bob == address(0) && carol == address(0), 'failed to register Alice');
-        emit LogRegisterAlice(msg.sender, _alice);
-        alice = _alice;
-        return true;
-    }
-    
-    function registerBob(address _bob) public returns (bool success){
-        require (alice != _bob && alice != address(0) && bob == address(0) && carol == address(0), 'failed to register Bob');
-        emit LogRegisterBob(msg.sender, _bob);
-        bob = _bob;
-        return true;
-    }
-    
-    function registerCarol(address _carol) public returns (bool success){
-        require (alice != _carol && bob != _carol && alice != address(0) && bob != address(0) && carol == address(0), 'failed to register Carol');
-        emit LogRegisterCarol(msg.sender, _carol);
-        carol = _carol;
-        return true;
-    }
-
-    function splitFundFromAlice() public payable returns(uint) {
-        require(alice == msg.sender && msg.value > 0, 'failed to get funds from alice');
+    function splitFunds(address toAddress1, address toAddress2) public payable returns(uint) {
+        require(msg.value > 0, 'failed to get funds from sender');
         uint oneWei = 1;
         uint amount = msg.value;
-        uint splitForBob = amount / 2;
-        uint splitForCarol = amount / 2;
+        uint splitForAddress1 = amount / 2;
+        uint splitForAddress2 = amount / 2;
         if (amount % 2 == 1){
-            require(splitForBob + oneWei > splitForBob, 'Overflow: splitForBob');
-            splitForBob += oneWei;
+            splitForAddress1 += oneWei;
         }
 
         // Check that funnds have been properly split
-        require(amount == splitForBob + splitForCarol, 'Split funds failed to add up');
+        require(amount == splitForAddress1 + splitForAddress2, 'Split funds failed to add up');
 
-        // Note: splitForBob >=1; splitForCarol >= 0;
-        require(bobFunds + splitForBob > bobFunds, 'Overflow: bobFunds');
-        require(carolFunds + splitForCarol >= carolFunds, 'Overflow: carolFunds');
-        emit LogSplitFunds(msg.sender, amount, splitForBob, splitForCarol);
-        bobFunds += splitForBob;
-        carolFunds += splitForCarol;
+        // Note: splitForAddress1 >=1; splitForAddress2 >= 0;
+        require(balances[toAddress1] + splitForAddress1 > balances[toAddress1], 'Overflow: balances[toAddress1]');
+        require(balances[toAddress2] + splitForAddress2 >= balances[toAddress2], 'Overflow: balances[toAddress2]');
+        emit LogSplitFunds(msg.sender, amount, splitForAddress1, splitForAddress2);
+        balances[toAddress1] += splitForAddress1;
+        balances[toAddress2] += splitForAddress2;
         return amount;
     }
 
-    function withdrawBob(uint amount) public returns (uint remainingBalance){
-        require(bob == msg.sender && bobFunds >= amount, 'failed to get request from bob');
-        bobFunds -= amount;
-        msg.sender.transfer(amount);
-        return bobFunds;
-    }
-
-    function withdrawCarol(uint amount) public returns(uint remainingBalance){
-        require(carol == msg.sender && carolFunds >= amount, 'failed to get request from carol');
-        carolFunds -= amount;
-        msg.sender.transfer(amount);
-        return carolFunds;
+    function withdrawFunds(uint amount_to_withdraw) public returns (uint){
+       require(balances[msg.sender] >= amount_to_withdraw);
+       balances[msg.sender] -= amount_to_withdraw;
+       msg.sender.transfer(amount_to_withdraw);
+       return balances[msg.sender];
     }
 
     function getContractBalance() public view returns(uint){
--- test/splitter.test.js
+++ test/splitter.test.js
@@ -1,65 +1,15 @@
 const Splitter = artifacts.require("Splitter");
 
-contract('Splitter - Initialization', (accounts) => {
-    it('Should initialize', async () => {
-        const splitterInstance = await Splitter.deployed();
-        const alice = await splitterInstance.alice();
-        const bob = await splitterInstance.bob();
-        const carol = await splitterInstance.carol();
-        const ZeroAddress = '0x0000000000000000000000000000000000000000';
-        assert.equal(alice, ZeroAddress, 'initial alice is incorrect');
-        assert.equal(bob, ZeroAddress, 'initial bob is incorrect');
-        assert.equal(carol, ZeroAddress, 'initial carol is incorrect');
-        
-    });
-    it('Should correctly register alice', async () => {
-        const ZeroAddress = '0x0000000000000000000000000000000000000000';
-        const splitterInstance = await Splitter.deployed();
-        await splitterInstance.registerAlice(accounts[1]);
-        const alice = await splitterInstance.alice();
-        
-        const bob = await splitterInstance.bob();
-        const carol = await splitterInstance.carol();
-        assert.equal(alice, accounts[1], 'registered alice is incorrect');
-        assert.equal(bob, ZeroAddress, 'initial bob is incorrect');
-        assert.equal(carol, ZeroAddress, 'initial carol is incorrect');        
-    });
-    it('Should correctly register alice and bob', async () => {
-        const ZeroAddress = '0x0000000000000000000000000000000000000000';
-        const splitterInstance = await Splitter.deployed();
-        const alice = await splitterInstance.alice();
-        await splitterInstance.registerBob(accounts[2]);
-        const bob = await splitterInstance.bob();
-        const carol = await splitterInstance.carol();
-        assert.equal(alice, accounts[1], 'registered alice is incorrect');
-        assert.equal(bob, accounts[2], 'registered bob is incorrect');
-        assert.equal(carol, ZeroAddress, 'initial carol is incorrect');        
-    });
-    it('Should correctly register alice, bob and carol', async () => {
-        const splitterInstance = await Splitter.deployed();
-        const alice = await splitterInstance.alice();
-        const bob = await splitterInstance.bob();
-        await splitterInstance.registerCarol(accounts[3]);
-        const carol = await splitterInstance.carol();
-        assert.equal(alice, accounts[1], 'registered alice is incorrect');
-        assert.equal(bob, accounts[2], 'registered bob is incorrect');
-        assert.equal(carol, accounts[3], 'registered carol is incorrect');        
-    });
-});
-
 contract('Splitter - split even funds', (accounts) => {
     it('Should split funds of 2 wei', async () => {
         const alice = accounts[4];
         const bob = accounts[5];
         const carol = accounts[6];
         const splitterInstance = await Splitter.deployed()
-        await splitterInstance.registerAlice(alice);
-        await splitterInstance.registerBob(bob);
-        await splitterInstance.registerCarol(carol);
         // Split 2 Wei
-        await splitterInstance.splitFundFromAlice({from: alice, value: web3.utils.toWei('2', 'wei')});
-        const bob_funds = await splitterInstance.bobFunds();
-        const carol_funds = await splitterInstance.carolFunds();
+        await splitterInstance.splitFunds(bob, carol, {from: alice, value: web3.utils.toWei('2', 'wei')});
+        const bob_funds = await splitterInstance.balances(bob);
+        const carol_funds = await splitterInstance.balances(carol);
         const contract_balance = await splitterInstance.getContractBalance();
         assert.equal(bob_funds.toString(10), '1');
         assert.equal(carol_funds.toString(10), '1');
@@ -73,13 +23,10 @@ contract('Splitter - split odd funds', (accounts) => {
         const bob = accounts[5];
         const carol = accounts[6];
         const splitterInstance = await Splitter.deployed();
-        await splitterInstance.registerAlice(alice);
-        await splitterInstance.registerBob(bob);
-        await splitterInstance.registerCarol(carol);
         // Split 1 Wei
-        await splitterInstance.splitFundFromAlice({from: alice, value: web3.utils.toWei('1', 'wei')});
-        const bob_funds = await splitterInstance.bobFunds();
-        const carol_funds = await splitterInstance.carolFunds();
+        await splitterInstance.splitFunds(bob, carol, {from: alice, value: web3.utils.toWei('1', 'wei')});
+        const bob_funds = await splitterInstance.balances(bob);
+        const carol_funds = await splitterInstance.balances(carol);
         const contract_balance = await splitterInstance.getContractBalance();
         //assert.equal(alice_deposit.toString(10), '1');
         assert.equal(bob_funds.toString(10), '1');
@@ -94,16 +41,13 @@ contract('Splitter - withdraw funds', (accounts) => {
         const bob = accounts[5];
         const carol = accounts[6];
         const splitterInstance = await Splitter.deployed();
-        await splitterInstance.registerAlice(alice);
-        await splitterInstance.registerBob(bob);
-        await splitterInstance.registerCarol(carol);
         // Split 1000 Wei
-        await splitterInstance.splitFundFromAlice({from: alice, value: web3.utils.toWei('1000', 'wei')});
-        await splitterInstance.withdrawBob('500', {from: bob});
-        const bob_funds = await splitterInstance.bobFunds();
+        await splitterInstance.splitFunds(bob, carol, {from: alice, value: web3.utils.toWei('1000', 'wei')});
+        await splitterInstance.withdrawFunds('500', {from: bob});
+        const bob_funds = await splitterInstance.balances(bob);
         assert.equal(bob_funds.toString(10), '0');
-        await splitterInstance.withdrawCarol('400', {from: carol});
-        carol_funds = await splitterInstance.carolFunds();
+        await splitterInstance.withdrawFunds('400', {from: carol});
+        carol_funds = await splitterInstance.balances(carol);
         assert.equal(carol_funds.toString(10), '100');
         const contract_balance = await splitterInstance.getContractBalance();
         assert.equal(contract_balance.toString(10), '100');
