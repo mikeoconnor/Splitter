c4e15af9a4a479adfd61dbdde177b6877b2416a7
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-24 10:01:55 +0100

    Add ability to stop contract in the case of emergencies
    The function stop() can be used to stop the contract. The function
    resume() can be used to resume a stopped contract.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,12 +3,25 @@ pragma solidity 0.5.0;
 contract Splitter {
 
     mapping (address => uint) public balances;
+    bool public isRunning = true;
     uint constant ONEWEI = 1;
 
     event LogSplitFunds(address sender, uint amount_to_split, address to_address1, address to_address2);
     event LogWithdrawFunds(address sender, uint amount_to_withdraw);
+    event LogStopped(address sender);
+    event LogResumed(address sender);
 
-    function splitFunds(address toAddress1, address toAddress2) public payable returns(bool success) {
+    modifier ifRunning {
+        require(isRunning);
+        _;
+    }
+
+    modifier ifStopped {
+        require(!isRunning);
+        _;
+    }
+
+    function splitFunds(address toAddress1, address toAddress2) public payable ifRunning returns(bool success) {
         require(msg.value > 0, 'failed to get funds from sender');
         require(balances[toAddress1] + (msg.value / 2) >= balances[toAddress1], 'Overflow: balances[toAddress1]');
         require(balances[toAddress2] + (msg.value / 2) >= balances[toAddress2], 'Overflow: balances[toAddress2]');
@@ -21,7 +34,7 @@ contract Splitter {
         return true;
     }
 
-    function withdrawFunds(uint amount_to_withdraw) public returns (bool success){
+    function withdrawFunds(uint amount_to_withdraw) public ifRunning returns (bool success){
        require(balances[msg.sender] >= amount_to_withdraw);
        emit LogWithdrawFunds(msg.sender, amount_to_withdraw);
        balances[msg.sender] -= amount_to_withdraw;
@@ -31,5 +44,17 @@ contract Splitter {
 
     function getContractBalance() public view returns(uint){
         return address(this).balance;
+    }
+
+    function stop() public ifRunning returns(bool success){
+        isRunning = false;
+        emit LogStopped(msg.sender);
+        return true;
+    }
+
+    function resume() public ifStopped returns(bool success){
+        isRunning = true;
+        emit LogResumed(msg.sender);
+        return true;
     }
 }
