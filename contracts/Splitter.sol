437f7213684686771ad008be65181b4bf0d48981
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-24 12:57:24 +0100

    Use SafeMath from openzeppelin in Splitter contract
    SafeMath extends uint so that mathematical operations are guarded
    against overflow and underflow.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,8 +1,11 @@
 pragma solidity 0.5.0;
 
 import "./Stoppable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Splitter is Stoppable {
+
+    using SafeMath for uint;
 
     mapping (address => uint) public balances;
     uint constant ONEWEI = 1;
@@ -12,22 +15,19 @@ contract Splitter is Stoppable {
 
     function splitFunds(address toAddress1, address toAddress2) public payable ifRunning returns(bool success) {
         require(msg.value > 0, 'failed to get funds from sender');
-        require(balances[toAddress1] + (msg.value / 2) >= balances[toAddress1], 'Overflow: balances[toAddress1]');
-        require(balances[toAddress2] + (msg.value / 2) >= balances[toAddress2], 'Overflow: balances[toAddress2]');
-        emit LogSplitFunds(msg.sender, msg.value, toAddress1, toAddress2);
-        balances[toAddress1] += msg.value / 2;
-        balances[toAddress2] += msg.value / 2;
+        balances[toAddress1] = balances[toAddress1].add(msg.value / 2);
+        balances[toAddress2] = balances[toAddress2].add(msg.value / 2);
         if (msg.value % 2 == 1){
-            balances[msg.sender] += ONEWEI;
+            balances[msg.sender] = balances[msg.sender].add(ONEWEI);
         }
+        emit LogSplitFunds(msg.sender, msg.value, toAddress1, toAddress2);
         return true;
     }
 
     function withdrawFunds(uint amount_to_withdraw) public ifRunning returns (bool success){
-       require(balances[msg.sender] >= amount_to_withdraw);
-       emit LogWithdrawFunds(msg.sender, amount_to_withdraw);
-       balances[msg.sender] -= amount_to_withdraw;
+       balances[msg.sender] = balances[msg.sender].sub(amount_to_withdraw);
        msg.sender.transfer(amount_to_withdraw);
+       emit LogWithdrawFunds(msg.sender, amount_to_withdraw);
        return true;
     }
 
