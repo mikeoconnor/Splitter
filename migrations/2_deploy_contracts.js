be1b7e615bb783fa85420a70a6f14a93485b4287
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-04-26 19:56:16 +0100

    Add Ownable.sol; encapsulates the owner of a contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Splitter = artifacts.require("Splitter");
+const Ownable = artifacts.require("Ownable");
 
 module.exports = function(deployer) {
+    deployer.deploy(Ownable);
     deployer.deploy(Splitter);
 };
